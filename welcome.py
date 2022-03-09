c2b1c20af8e9d1b2ce606ff3846f547e4d01e840
vbkulkarni <73324381+vbkulkarni@users.noreply.github.com>
2022-03-09 07:39:37 +0530

    Added Find Even No Code

--- welcome.py
+++ welcome.py
@@ -1,2 +1,10 @@
 
 print('Welcome to python programming language')
+
+//following program find for given no is even 
+
+num = int (input (“Enter any number to test whether it is odd or even: “)
+
+if (num % 2) == 0:
+
+              print (“The number is even”)
