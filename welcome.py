1779fa3b87ee915bbb79593d7a8b5e17f11a432f
vbkulkarni <73324381+vbkulkarni@users.noreply.github.com>
2022-03-10 07:21:52 +0530

    revise code

--- welcome.py
+++ welcome.py
@@ -8,5 +8,3 @@ num = int (input (“Enter any number to test whether it is odd or even: “)
 if (num % 2) == 0:
 
               print (“The number is even”)
-else
-	print("The number is odd") // odd Statement
